5a0d961acab00a366367c2acd2dbb9508583d743
HAYASHI Masayuki <masayuki.hayashi.mcc@gmail.com>
2023-02-25 14:00:13 +0900

    add: 単純にレスポンスを返すだけのAPIを実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+    get '/echo', to: 'echo#index'
+  end
 end
