3376887ac842bfc3d49640c3ada2fe3215fd4605
HAYASHI Masayuki <masayuki.hayashi.mcc@gmail.com>
2023-07-17 17:16:02 +0900

    アカウントモデルのテストで使うべきメソッドを使用していなかった
    3adbad993a6f0f38ebc3b4837f0346c04c65148b でOpenIDプロバイダを別モデルと
    して切り離した際に、モデルの実装とテストの実装を別の形で修正してしまって
    いた。

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -2,28 +2,25 @@ require 'rails_helper'
 
 RSpec.describe Account do
   it 'is valid with open id provider' do
-    account = described_class.new
-    account.open_id_providers.new(sub: '111111111111111111111', provider: 'google')
+    account = described_class.create_with_open_id_provider('111111111111111111111', 'google')
 
     expect(account).to be_valid
   end
 
   it 'is invalid without open id provider sub' do
-    account = described_class.new
-    account.open_id_providers.new(provider: 'google')
+    account = described_class.create_with_open_id_provider('', 'google')
 
     expect(account).not_to be_valid
   end
 
   it 'is invalid without open id provider provider' do
-    account = described_class.new
-    account.open_id_providers.new(sub: '111111111111111111111')
+    account = described_class.create_with_open_id_provider('111111111111111111111', '')
 
     expect(account).not_to be_valid
   end
 
   it 'is invalid without open id provider' do
-    account = described_class.new
+    account = described_class.create_with_open_id_provider('', '')
 
     expect(account).not_to be_valid
   end
