4e7b7221fa4d6cc1018eb680ee6a65ea1ab18b25
HAYASHI Masayuki <masayuki.hayashi.mcc@gmail.com>
2023-07-17 17:20:29 +0900

    テストがどのモデルをテストしているかをdescribeで説明するように

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -1,27 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Account do
-  it 'is valid with open id provider' do
-    account = described_class.create_with_open_id_provider('111111111111111111111', 'google')
+  describe '.create_with_open_id_provider' do
+    it 'is valid with open id provider' do
+      account = described_class.create_with_open_id_provider('111111111111111111111', 'google')
 
-    expect(account).to be_valid
-  end
+      expect(account).to be_valid
+    end
 
-  it 'is invalid without open id provider sub' do
-    account = described_class.create_with_open_id_provider('', 'google')
+    it 'is invalid without open id provider sub' do
+      account = described_class.create_with_open_id_provider('', 'google')
 
-    expect(account).not_to be_valid
-  end
+      expect(account).not_to be_valid
+    end
 
-  it 'is invalid without open id provider provider' do
-    account = described_class.create_with_open_id_provider('111111111111111111111', '')
+    it 'is invalid without open id provider provider' do
+      account = described_class.create_with_open_id_provider('111111111111111111111', '')
 
-    expect(account).not_to be_valid
-  end
+      expect(account).not_to be_valid
+    end
 
-  it 'is invalid without open id provider' do
-    account = described_class.create_with_open_id_provider('', '')
+    it 'is invalid without open id provider' do
+      account = described_class.create_with_open_id_provider('', '')
 
-    expect(account).not_to be_valid
+      expect(account).not_to be_valid
+    end
   end
 end
