dc0fbb0f96df0af76653d794a863241e48962adb
haruya fujimoto <hal2cilu@gmail.com>
2023-07-23 14:46:11 +0900

    task_groups, task_categories にて migration file 作成、migrate 実行

--- db/migrate/20230722093946_create_task_categories.rb
+++ db/migrate/20230722093946_create_task_categories.rb
@@ -1,7 +1,8 @@
 class CreateTaskCategories < ActiveRecord::Migration[7.0]
   def change
     create_table :task_categories do |t|
-
+      t.references :task_group, null: false
+      t.string :name, null: false, default: ''
       t.timestamps
     end
   end
