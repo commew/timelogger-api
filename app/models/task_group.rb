5d00fc5eb6badee15b66ebde62f25e8f5873b2a5
haruya fujimoto <hal2cilu@gmail.com>
2023-07-23 23:01:01 +0900

    TaskGroup, TaskCategory の仮実装
    - TaskGroup は Account と関連があるが、Account は別 issue で対応中のため関連の記述をコメントアウト
    - TaskGroup 生成時に TaskCategory が自動生成される処理を実装
    - 上記のテストを記述
    - TaskGroup を削除時に TaskCategory が削除されるかのテストを記述

--- app/models/task_group.rb
+++ app/models/task_group.rb
@@ -1,2 +1,46 @@
 class TaskGroup < ApplicationRecord
+  # 仮りで初期生成される task_group と task_categories
+  INIT_DATA = {
+    '仕事' => [
+      {
+        name: '会議'
+      },
+      {
+        name: '資料作成'
+      }
+    ],
+    '学習' => [
+      {
+        name: 'TOEIC'
+      }
+    ],
+    '趣味' => [
+      {
+        name: '散歩'
+      },
+      {
+        name: '読書'
+      }
+    ],
+    'グループ未分類' => [
+      {
+        name: '移動・外出'
+      }
+    ]
+  }.freeze
+
+  ## account table は別 issue で対応のため、今はコメントアウト
+  # belongs_to :account
+  # validates :account, presence: true
+  has_many :task_categories, dependent: :destroy
+
+  after_create :create_categories
+
+  private
+
+  def create_categories
+    INIT_DATA[name].each do |category|
+      task_categories.create(category)
+    end
+  end
 end
--- spec/models/task_group_spec.rb
+++ spec/models/task_group_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe TaskGroup do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # TasdGroup,TaskCategory のデフォルト値が変わる可能性があるため、
+  # テストにハードコードしない目的の記述
+  sample_task_group_name = described_class::INIT_DATA.keys.first
+  sample_task_category_names = described_class::INIT_DATA[sample_task_group_name].pluck(:name)
+
+  # #create_categories is registerd to "after_create"
+  describe '#create_categories' do
+    context "when created task_group's name prop is \"#{sample_task_group_name}\"" do
+      let(:task_group) { described_class.create(name: sample_task_group_name) }
+      let(:task_category_names) do
+        described_class::INIT_DATA[sample_task_group_name].pluck(:name)
+      end
+
+      it 'affect to create task_categories.' do
+        expect(task_group.task_categories.pluck(:name)).to eq(sample_task_category_names)
+      end
+    end
+  end
 end
