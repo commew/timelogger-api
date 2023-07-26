197487a039943c203cf00b63c869d7ad27c986ca
haruya fujimoto <hal2cilu@gmail.com>
2023-07-27 00:32:28 +0900

    TaskGroup のコントローラ、ルーティング等を仮実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :accounts, only: [:create]
-
+  resources :task_groups, only: [:index], path: "task-groups"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
