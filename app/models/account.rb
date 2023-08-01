97e514120dd8ee684683454d85232d23ccba398c
haruya fujimoto <hal2cilu@gmail.com>
2023-08-01 22:14:55 +0900

    update models: Account, TaskGroup にて関連と機能追加・調整

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,12 +1,19 @@
 class Account < ApplicationRecord
   has_many :open_id_providers, dependent: :destroy
+  has_many :task_groups, dependent: :destroy
 
   validates :open_id_providers, presence: true
+
+  after_create :create_task_groups
 
   def self.create_with_open_id_provider(sub, provider)
     new.tap do |account|
       account.open_id_providers.new(sub:, provider:)
       account.save
     end
+  end
+
+  def create_task_groups
+    task_groups << TaskGroup.default_tasks
   end
 end
