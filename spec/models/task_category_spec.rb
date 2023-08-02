f801c66ca5cf55e919a14fb9fb019ab6721118d4
haruya fujimoto <hal2cilu@gmail.com>
2023-08-02 22:14:56 +0900

    add: TaskCategory のテストを追加(#as_json だけ)

--- spec/models/task_category_spec.rb
+++ spec/models/task_category_spec.rb
@@ -1,1 +1,19 @@
 require 'rails_helper'
+
+RSpec.describe TaskCategory do
+  describe '#as_json' do
+    context 'when there is a' do
+      let(:task_category) { create(:task_category, name: '会議') }
+      let(:expected) do
+        {
+          'id' => task_category.id,
+          'name' => task_category.name
+        }
+      end
+
+      it 'returns expected hash no including timestamps.' do
+        expect(task_category.as_json).to eq(expected)
+      end
+    end
+  end
+end
