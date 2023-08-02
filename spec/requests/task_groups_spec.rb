1e12c2f835c7c20b51c0d26d6de56824a73854e2
haruya fujimoto <hal2cilu@gmail.com>
2023-08-02 22:17:54 +0900

    modify: TaskGroupsController のテストの調整

--- spec/requests/task_groups_spec.rb
+++ spec/requests/task_groups_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'TaskGroups' do
+  # ここでは正常系のみテストをする
   describe 'GET /task-groups' do
     before do
       get '/task-groups'
@@ -11,74 +12,19 @@ RSpec.describe 'TaskGroups' do
     end
 
     context 'when success, inspect body schema' do
-      ## 現時点での期待されるレスポンスのスキーマ
-      # {
-      #   "groups": [
-      #     {
-      #       "id": 1,
-      #       "name": "仕事",
-      #       "categories": [
-      #         {
-      #           "id": 1,
-      #           "name": "会議"
-      #         },
-      #         {
-      #           "id": 2,
-      #           "name": "資料作成"
-      #         }
-      #       ]
-      #     },
-      #     {
-      #       "id": 2,
-      #       "name": "学習",
-      #       "categories": [
-      #         {
-      #           "id": 3,
-      #           "name": "TOEIC"
-      #         }
-      #       ]
-      #     },
-      #     {
-      #       "id": 3,
-      #       "name": "趣味",
-      #       "categories": [
-      #         {
-      #           "id": 4,
-      #           "name": "散歩"
-      #         },
-      #         {
-      #           "id": 5,
-      #           "name": "読書"
-      #         }
-      #       ]
-      #     },
-      #     {
-      #       "id": 4,
-      #       "name": "グループ未分類",
-      #       "categories": [
-      #         {
-      #           "id": 6,
-      #           "name": "移動・外出"
-      #         }
-      #       ]
-      #     }
-      #   ]
-      # }
       let(:result) { JSON.parse(response.body) }
-      let(:top_level_keys) { result.keys }
-      let(:categories_keys) { result[top_level_keys.first].first['categories'].first.keys }
-      let(:second_level_keys) { result[top_level_keys.first].first.keys }
+      # 本来は authorization で account を特定するが、
+      # 現在は未実装なのでこのようにテストを書いている
+      let(:account) { Account.first }
+      # as_json の返す値の正しさは、TaskGroup, TaskCatetory でのテスト対象
+      let(:expected) {
+        {
+          "groups" => account.task_groups.as_json
+        }
+      }
 
-      it 'top level keys is ["groups"]' do
-        expect(top_level_keys).to eq(['groups'])
-      end
-
-      it 'second level keys is ["id", "name", "categories"]' do
-        expect(second_level_keys).to eq(%w[id name categories])
-      end
-
-      it 'categories keys is ["id", "name"]' do
-        expect(categories_keys).to eq(%w[id name])
+      it 'returns expected hash.' do
+        expect(result).to eq(expected)
       end
     end
   end
