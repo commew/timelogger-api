35e4be50fc116adfabf688e25c40f31fc2a8fdfa
haruya fujimoto <hal2cilu@gmail.com>
2023-08-12 17:51:49 +0900

    modify: コールバックを削除したことに伴い、一部の factory を改修

--- spec/factories/task_groups.rb
+++ spec/factories/task_groups.rb
@@ -1,21 +1,6 @@
 FactoryBot.define do
   factory :task_group do
     name { '仕事' }
-    account { association :account, skip_callback: true }
-
-    # TaskGroup は create されると TaskCategory も自動で作られる
-    # TaskCategory 側のテストで困ることがあるので、
-    # callback を一時的に skip した状態で TaskGroup を作成することもできるようにする
-    transient do
-      skip_callback { false }
-    end
-
-    after(:build) do |task_group, evaluator|
-      task_group.class.skip_callback(:create, :after, :create_categories) if evaluator.skip_callback
-    end
-
-    after(:create) do |task_group, evaluator|
-      task_group.class.set_callback(:create, :after, :create_categories) if evaluator.skip_callback
-    end
+    account { association :account }
   end
 end
