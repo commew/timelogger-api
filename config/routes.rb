94dc144c22fcd35ee1611d27e73a336945a7bf0c
haruya fujimoto <hal2cilu@gmail.com>
2023-08-24 21:19:19 +0900

    add: エンドポイント定義書にある、全てのエンドポイントを仮定義

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   resources :accounts, only: [:index, :create]
   resources :task_groups, only: [:index], path: "task-groups"
+  resources :tasks, only: [:create] do
+    member do
+      patch 'stop'
+      patch 'complete'
+    end
+  end
+  get 'tasks/recording', 'tasks#recording'
+  get 'tasks/pending', 'tasks#pending'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
