ed3b0ce2066acbd15b93e7c2fe73ad92187022cc
haruya fujimoto <hal2cilu@gmail.com>
2023-09-03 20:13:38 +0900

    modify: task と task_time_unit の factory を役に立つものに調整

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -2,5 +2,6 @@ FactoryBot.define do
   factory :task do
     completed { false }
     task_category { association :task_category }
+    task_time_units { [FactoryBot.build(:task_time_unit)] }
   end
 end
