b9f1717b159621dce05d7e023652a40391a160dc
haruya fujimoto <hal2cilu@gmail.com>
2023-09-09 20:42:20 +0900

    revert: controller 周りの変更を dev の状態に戻した

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,6 @@
 class TasksController < ApplicationController
   skip_before_action :authenticate
   def create
-    task_category = TaskCategory.find params[:taskCategoryId]
-    return render_invalid_task_group_task_category if params[:taskGroupId] == tas_category.task_group.id
-
-    task = Task.start_recording task_category, params[:startAt]
-    render_start_task_recording task
     render json: {}, status: :ok
   end
 
@@ -23,25 +18,5 @@ class TasksController < ApplicationController
 
   def complete
     render json: {}, status: :ok
-  end
-
-  private
-
-  def create_params
-    params.permit(:taskGroupId, :taskCategoryId, :status, :startAt)
-  end
-
-  def render_invalid_task_group_task_category; end
-
-  def render_start_task_recording(task)
-    {
-      id: task.id,
-      status: task.status,
-      startAt: task.start_at,
-      endAt: task.end_at,
-      duration: 14_400,
-      taskGroupId: 1,
-      taskCategoryId: 1
-    }
   end
 end
--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -4,51 +4,19 @@ require 'rails_helper'
 RSpec.describe 'Tasks' do
   describe 'POST /tasks' do
     before do
-      post '/tasks', headers:, params:
+      post '/tasks'
     end
 
-    let(:task_category) { create(:task_category) }
-
-    let(:open_id_provider) do
-      {
-        sub: '111111111111111111111',
-        provider: 'google'
-      }
-    end
-    let(:account) do
-      # factory ではなく .create_with_open_id_provider により
-      # TaskGroup を初期でセットする
-      Account.create_with_open_id_provider(**open_id_provider)
-    end
-    let(:headers) do
-      token = JWT.encode(open_id_provider, Rails.application.config_for(:auth)[:jwt_hmac_secret])
-      {
-        Authorization: "Bearer #{token}"
-      }
+    it 'returns 200 as dummy.' do
+      expect(response).to have_http_status(200)
     end
 
-    context 'when success' do
-      let(:params) do
-        {
-          taskGroupId: task_category.task_group.id,
-          taskCategoryId: task_category.id,
-          status: 'recording',
-          startAt: '2019-08-24T14:15:22Z'
-        }
-      end
-
-      it 'returns 201.' do
-        expect(response).to have_http_status(201)
-      end
-
-      it 'returns empty json' do
-        p response.body
-        expect(response.body).to eq('{}')
-      end
+    it 'returns empty json' do
+      expect(response.body).to eq('{}')
     end
   end
 
-  describe 'PATCH /tasks/:id/stop', skip: 'not implemented yet' do
+  describe 'PATCH /tasks/:id/stop' do
     before do
       patch '/tasks/1/stop'
     end
@@ -62,7 +30,7 @@ RSpec.describe 'Tasks' do
     end
   end
 
-  describe 'PATCH /tasks/:id/complete', skip: 'not implemented yet' do
+  describe 'PATCH /tasks/:id/complete' do
     before do
       patch '/tasks/2/complete'
     end
@@ -76,7 +44,7 @@ RSpec.describe 'Tasks' do
     end
   end
 
-  describe 'GET /tasks/recording', skip: 'not implemented yet' do
+  describe 'GET /tasks/recording' do
     before do
       get '/tasks/recording'
     end
@@ -90,7 +58,7 @@ RSpec.describe 'Tasks' do
     end
   end
 
-  describe 'GET /tasks/pending', skip: 'not implemented yet' do
+  describe 'GET /tasks/pending' do
     before do
       get '/tasks/pending'
     end
