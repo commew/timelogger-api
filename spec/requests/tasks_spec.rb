ba61b7d3cb89c1b97b32244e83ce722956bc8e67
HAYASHI Masayuki <masayuki.hayashi.mcc@gmail.com>
2023-10-05 17:27:37 +0900

    タスクの記録を開始するAPIの正常系のテストを実装

--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -7,16 +7,18 @@ RSpec.describe 'Tasks' do
 
     let(:account) { create(:account, task_groups: [task_category.task_group]) }
 
-    let(:params) do
-      {
-        taskGroupId: task_category.task_group.id,
-        taskCategoryId: task_category.id,
-        status: Task::STATUS[:recording]
-      }
-    end
-
     context 'when task created' do
+      let(:now) { Time.zone.now }
+
       before do
+        params = {
+          taskGroupId: task_category.task_group.id,
+          taskCategoryId: task_category.id,
+          status: Task::STATUS[:recording]
+        }
+
+        travel_to now
+
         post '/tasks', params:, headers: headers(account)
       end
 
@@ -24,7 +26,33 @@ RSpec.describe 'Tasks' do
         expect(response).to have_http_status(201)
       end
 
-      # TODO
+      it 'returns task id' do
+        expect(JSON.parse(response.body)['id']).not_to be_nil
+      end
+
+      it 'returns recording as status' do
+        expect(JSON.parse(response.body)['status']).to eq Task::STATUS[:recording].to_s
+      end
+
+      it 'returns current time as startAt' do
+        expect(JSON.parse(response.body)['startAt']).to eq now.rfc3339
+      end
+
+      it 'returns nil as endAt' do
+        expect(JSON.parse(response.body)['endAt']).to eq '0000-00-00T00:00:00Z'
+      end
+
+      it 'returns 0 as duration' do
+        expect(JSON.parse(response.body)['duration']).to be 0
+      end
+
+      it 'returns task group id' do
+        expect(JSON.parse(response.body)['taskGroupId']).to be task_category.task_group.id
+      end
+
+      it 'returns task category id' do
+        expect(JSON.parse(response.body)['taskCategoryId']).to be task_category.id
+      end
     end
 
     # TODO: 異常系も
